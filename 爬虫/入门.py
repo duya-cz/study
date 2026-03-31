b3597214480b085e132fc72521b6da6575d9f20b
cz <17671297284@163.com>
2026-03-31 22:27:25 +0800

    没学多少，看新闻去了

--- 爬虫/入门.py
+++ 爬虫/入门.py
@@ -1,25 +1,23 @@
+import requests
 from lxml import html
 
-# 读取html文件
-with open('resources\code.html', 'r', encoding='utf-8') as f:
-    html_text = f.read()
-    print(html_text) 
+# 定义网址
+target_url="https://www.tiobe.com/tiobe-index/"
 
-    # 解析文本，转换为一个文档对象
-    document=html.fromstring(html_text) 
+# 发送请求获取网页内容
+response=requests.get(target_url)
 
-    # 解析表头，-xpath语法
-    th_list=document.xpath('//table/thead/tr/th/text()')
-    print(th_list)
+# 输出数据到控制台
+document=html.fromstring(response.text)
 
-    # 获取表格内第一条数据
-    td_list=document.xpath('//table/tbody/tr[1]/td/text()')
+# 解析数据
+th_list=document.xpath('//table[@id="top20"]/thead/tr/th/text()')
+# 输出数据
+print(th_list)
+
+# 解析表格数据
+tr_list=document.xpath('//table[@id="top20"]/tbody/tr')
+# 输出数据每条数据每列输出
+for tr in tr_list:
+    td_list=tr.xpath('./td/text()')
     print(td_list)
-
-    # 获取表格内所有数据，每条元素每行显示
-    tr_list=document.xpath('//table/tbody/tr')
-
-    for tr in tr_list:
-        tr_list_td=tr.xpath('./td/text()')
-        print(tr_list_td)
-        
